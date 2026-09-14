2893396e76426ab363fbadace643093f33ba7ce4
minh nguyene <yellowwheat@gmail.com>
2026-09-14 15:35:38 -0700

    Work on wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,33 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if not title or not genre or not rating:
+        return None
+
+    return {
+        "title": title,
+        "genre": genre,
+        "rating": round(rating, 2)
+    }
+    
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+  
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+  
+    return user_data
+
+def watch_movie(user_data, title): 
+
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watched"].append(movie)
+            user_data["watchlist"].remove(movie)
+
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
